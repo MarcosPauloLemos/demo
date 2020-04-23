243c47a4d1676163dec40b5aac1f84d3aa2bd3c2
s2it_mlemos <s2it_mlemos@uolinc.com>
2020-04-23 03:28:45 -0300

    ADD jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+pipeline {
+    stages {
+        stage ('checkout') {
+            sh 'git pull origin master'
+        }
+
+        stage ('build') {
+            sh './gradlew clean build -X test'
+        }
+
+        stage ('test') {
+            sh './gradlew test'
+        }
+
+        stage ('docker') {
+            sh 'docker build -t localhost:5000/demo:1 .'
+            sh 'docker push localhost:5000/demo:1'
+        }
+
+        stage ('deploy') {
+            sh 'docker run -d -p 9081:9081 localhost:5000/demo:1'
+        }
+    }
+}
